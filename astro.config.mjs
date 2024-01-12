b919779a4ec7228a47a8cd88c64dbe9003db34a4
Devessier <baptiste@devessier.fr>
2024-01-12 15:22:59 +0100

    feat: set up expressive code for syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: ["stately.ai"]
   },
-  integrations: [mdx(), react()]
+  integrations: [expressiveCode(), mdx(), react()]
 });
