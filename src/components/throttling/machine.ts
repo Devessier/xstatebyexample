e0c6952d473034d24107832a1561409b86adbead
Devessier <baptiste@devessier.fr>
2024-01-14 12:10:38 +0100

    refactor: use setup function instead of createMachine

--- src/components/throttling/machine.ts
+++ src/components/throttling/machine.ts
@@ -1,46 +1,42 @@
-import { assign, createMachine } from "xstate";
+import { assign, setup } from "xstate";
 
-export const throttlingMachine = createMachine(
-  {
-    id: "Throttling",
-    context: {
+export const throttlingMachine = setup({
+  types: {
+    events: {} as { type: "click" } | { type: "reset" },
+    context: {} as { counter: number },
+  },
+  actions: {
+    "Increment counter": assign({
+      counter: ({ context }) => context.counter + 1,
+    }),
+    "Reset counter": assign({
       counter: 0,
-    },
-    initial: "Idle",
-    states: {
-      Idle: {
-        on: {
-          click: {
-            target: "Throttling",
-          },
-          reset: {
-            actions: "Reset counter",
-          },
+    }),
+  },
+}).createMachine({
+  id: "Throttling",
+  context: {
+    counter: 0,
+  },
+  initial: "Idle",
+  states: {
+    Idle: {
+      on: {
+        click: {
+          target: "Throttling",
         },
-      },
-      Throttling: {
-        after: {
-          "1000": {
-            target: "Idle",
-            actions: "Increment counter",
-          },
+        reset: {
+          actions: "Reset counter",
         },
       },
     },
-    types: {
-      events: {} as { type: "click" } | { type: "reset" },
-      context: {} as { counter: number },
-      actions: {} as { type: "Increment counter" } | { type: "Reset counter" },
+    Throttling: {
+      after: {
+        "1000": {
+          target: "Idle",
+          actions: "Increment counter",
+        },
+      },
     },
   },
-  {
-    actions: {
-      "Increment counter": assign({
-        counter: ({ context }) => context.counter + 1,
-      }),
-      "Reset counter": assign({
-        counter: 0,
-      }),
-    },
-  }
-);
+});
