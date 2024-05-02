6369a84a87949e34dc133d22e44295e7533d5e8c
Devessier <baptiste@devessier.fr>
2024-05-02 14:13:42 +0200

    feat: make all buttons have a pointer cursor

--- panda.config.ts
+++ panda.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from "@pandacss/dev";
 import typographyPreset from "pandacss-preset-typography";
-import pandaPreset from '@pandacss/preset-panda';
-import formPreset from 'pform-reset';
+import pandaPreset from "@pandacss/preset-panda";
+import formPreset from "pform-reset";
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ['./src/**/*.{ts,tsx,js,jsx,astro,mdx}'],
+  include: ["./src/**/*.{ts,tsx,js,jsx,astro,mdx}"],
 
   // Files to exclude
   exclude: [],
+
+  globalCss: {
+    button: {
+      cursor: "pointer",
+    },
+  },
 
   // Useful for theme customization
   theme: {
@@ -20,15 +26,15 @@ export default defineConfig({
         /**
          * Comes from https://github.com/fkhadra/react-toastify/blob/edb231d07cc298a82e26d489030356387906ff92/scss/_progressBar.scss#L1-L8.
          */
-        'progress-bar': {
-          '0%': {
-            transform: "scaleX(1)"
+        "progress-bar": {
+          "0%": {
+            transform: "scaleX(1)",
           },
-          '100%': {
-            transform: "scaleX(0)"
+          "100%": {
+            transform: "scaleX(0)",
           },
-        }
-      }
+        },
+      },
     },
   },
 
@@ -39,9 +45,9 @@ export default defineConfig({
     typographyPreset({
       recipe: {
         notProse: true,
-      }
+      },
     }),
     pandaPreset,
     formPreset,
-  ]
+  ],
 });
